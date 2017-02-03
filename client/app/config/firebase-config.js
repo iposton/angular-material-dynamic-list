0370d711b913a02d6e95ad96c007e4bec3d7ae63
iposton <ianposton@sbcglobal.net>
2017-02-02 17:16:34 -0800

    add heroku keys to index

--- client/app/config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-apiKey: API_KEY,
-authDomain: AUTH_DOM,
-databaseURL: FIREBASE_URL,
-storageBucket: STRG_BUCKET,
-messagingSenderId: MSG_SND_ID
-};
-firebase.initializeApp(config);
