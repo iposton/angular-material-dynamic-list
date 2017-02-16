d98f8300dd8b85cea947d25ca30f9c46af418393
iposton <ianposton@sbcglobal.net>
2017-02-15 18:00:41 -0800

    clean up main container directive and view

--- client/app/components/main-container/maincontainer-directive.js
+++ client/app/components/main-container/maincontainer-directive.js
@@ -5,42 +5,49 @@
         .module("lessons")
         .directive("mainContainer", function() {
             return {
-                 scope: {
+                scope: {
                     currentuser: '=currentuser'
                 },
                 templateUrl: "components/main-container/main-container.html",
                 controller: mainContainerCtrl,
-                controllerAs: "ll"
+                controllerAs: "vm"
             }
 
             function mainContainerCtrl(lessonService, $mdBottomSheet, $mdSidenav, $scope, $timeout, $mdDialog) {
- 
+
                 var self = this;
 
+                // Set globals
                 self.selected = null;
-                $scope.tweet = null;
-                $scope.lesson = null;
-            
-                self.lessons = [];
+                self.tweet = null;
+                
+
+                // Define functions
+                self.getTweet = getTweet;
+                self.showEdit = showEdit;
                 self.selectLesson = selectLesson;
                 self.setActive = setActive;
                 self.deleteLesson = deleteLesson;
+                self.showNew = showNew;
+                self.clearForm = clearForm;
+                self.saveLesson = saveLesson;
 
                 // PULL IN DATA FROM FIREBASE
                 self.lessons = lessonService.ref;
 
+                // For $mdDialog
                 var parentEl = angular.element(document.body);
 
                 function selectLesson(lesson) {
                     self.selected = lesson;
                     $mdBottomSheet.hide(self.selected);
                     $mdSidenav('left').toggle();
-                    $scope.getTweet();
-                    
+                    self.getTweet();
+
                 }
 
                 function setActive(item, list) {
-                    
+
                     list.some(function(item) {
 
                         if (item.active) {
@@ -51,98 +58,105 @@
 
                 }
 
-                $scope.getTweet = function () {
-                    $scope.tweet = "Check out " + self.selected.name + " at " + self.selected.websiteUrl + ". More info about where to find coding lessons.";
+                function getTweet() {
+                    self.tweet = "Check out " + self.selected.name + " at " + self.selected.websiteUrl + ". More info about where to find coding lessons.";
                 }
 
-                $scope.showEdit = function($event) {
-                        //$scope.getCurrentUser();
-                        $scope.editing = true;
-                            $mdDialog.show({
-                              
-                                locals:{
-                                    lesson: self.selected
-                                },
-                                controller: ['$scope', 'lesson', 'lessonService', function($scope, lesson, lessonService) {
-                                    var self = this;
-                                    self.selected = lesson;
-                                    self.id = null;
-                                    self.lessons = lessonService.ref;
+                function showEdit($event) {
 
-                                    $scope.saveEdit = function (l) {
-                                        self.id = l.$id
-                                        self.editedLesson = self.lessons.$getRecord(self.id);
-                                        self.lessons.$save(self.editedLesson);
-                                        self.selected = {};
-                                        $mdDialog.hide();
+                    $mdDialog.show({
 
-                                    }
+                        locals: {
+                            lesson: self.selected
+                        },
+                        controller: ['$scope', 'lesson', 'lessonService', function($scope, lesson, lessonService) {
+                            var self = this;
+                            self.selected = lesson;
+                            self.id = null;
+                            self.lessons = lessonService.ref;
 
-                                    $scope.clearForm = function() {
-                                        $mdDialog.hide();
-                                    }
-                                }],
-                                controllerAs: 'vm',
-                                parent: parentEl,
-                                targetEvent: $event,
-                                templateUrl: 'components/editModal.html',
-                                
-                            });  
-                        
-                    }
+                            self.saveEdit = saveEdit;
+                            self.clearForm = clearForm;
 
-                        
-                    $scope.showNew = function($event) {
-                            $mdDialog.show({
-                                parent: parentEl,
-                                targetEvent: $event,
-                                templateUrl: 'components/newModal.html',
-                                preserveScope: true,
-                                controller: mainContainerCtrl
-                            }); 
-                    }
+                            function saveEdit(l) {
+                                // don't save these values only for view condition
+                                // TODO: Find a better way to do this
+                                l.voted = null;
+                                l.votedValue = null;
+                                self.id = l.$id
+                                self.editedLesson = self.lessons.$getRecord(self.id);
+                                self.lessons.$save(self.editedLesson);
+                                self.selected = {};
+                                $mdDialog.hide();
 
-                    // Add a new lesson
-                    $scope.saveLesson = function(lesson) {
-                        self.lessons.$add(lesson);
-                        $mdDialog.hide();
-                    }
+                            }
 
-                    $scope.clearForm = function() {
-                        $scope.lessons = {like: 0, dislike: 0, id: 100};
-                    }
+                            function clearForm() {
+                                $mdDialog.hide();
+                            }
+                        }],
+                        controllerAs: 'vm',
+                        parent: parentEl,
+                        targetEvent: $event,
+                        templateUrl: 'components/editModal.html',
 
-                   function deleteLesson(event, lesson) {
-                      var confirm = $mdDialog.confirm()
-                          .title("Are you sure you want to delete " + lesson.name + "?")
-                          .ok("Yes")
-                          .cancel("No")
-                          .targetEvent(event);
-                      $mdDialog.show(confirm).then(function() {
-                          self.lessons.$remove(lesson);
-                          //showToast(lesson.names + ' Deleted!');
-                      }, function() {
+                    });
 
-                      });
-                  }
+                }
+
+
+                function showNew($event) {
+                    $mdDialog.show({
+                        parent: parentEl,
+                        targetEvent: $event,
+                        templateUrl: 'components/newModal.html',
+                        preserveScope: true,
+                        controller: mainContainerCtrl,
+                        controllerAs: 'vm'
+                    });
+                }
+
+                // Add a new lesson
+                function saveLesson(lesson) {
+                    self.lessons.$add(lesson);
+                    $mdDialog.hide();
+                }
+
+                function clearForm() {
+                    self.newLesson = { like: 0, dislike: 0, id: 100 };
+                }
+
+                function deleteLesson(event, lesson) {
+                    var confirm = $mdDialog.confirm()
+                        .title("Are you sure you want to delete " + lesson.name + "?")
+                        .ok("Yes")
+                        .cancel("No")
+                        .targetEvent(event);
+                    $mdDialog.show(confirm).then(function() {
+                        self.lessons.$remove(lesson);
+                        //showToast(lesson.names + ' Deleted!');
+                    }, function() {
+
+                    });
+                }
 
 
             }
-        }).directive('onLoadClicker', function ($timeout) {
-                return {
-                    restrict: 'A',
-                    scope: {
-                        index: '=index'
-                    },
-                    link: function($scope, iElm) {
-                        if ($scope.index === 0) {
-                            $timeout(function() {
+        }).directive('onLoadClicker', function($timeout) {
+            return {
+                restrict: 'A',
+                scope: {
+                    index: '=index'
+                },
+                link: function($scope, iElm) {
+                    if ($scope.index === 0) {
+                        $timeout(function() {
 
-                                iElm.triggerHandler('click');
+                            iElm.triggerHandler('click');
 
-                            }, 0);
-                        }
+                        }, 0);
                     }
-                };
-            });
+                }
+            };
+        });
 }());
